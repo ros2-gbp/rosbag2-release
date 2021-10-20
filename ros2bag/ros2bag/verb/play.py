c805e3cfaa682f13de7d0dd775af92cf984fd19d
Emerson Knapp <emerson.b.knapp@gmail.com>
2021-10-19 22:40:58 -0700

    Rebase from 'upstream'
    Signed-off-by: Emerson Knapp <emerson.b.knapp@gmail.com>

--- ros2bag/ros2bag/verb/play.py
+++ ros2bag/ros2bag/verb/play.py
@@ -82,6 +82,12 @@ class PlayVerb(VerbExtension):
             '--clock', type=positive_float, nargs='?', const=40, default=0,
             help='Publish to /clock at a specific frequency in Hz, to act as a ROS Time Source. '
                  'Value must be positive. Defaults to not publishing.')
+        parser.add_argument(
+            '-d', '--delay', type=positive_float, default=0.0,
+            help='Sleep duration before play (each loop), in seconds. Negative durations invalid.')
+        parser.add_argument(
+            '--disable-keyboard-controls', action='store_true',
+            help='disables keyboard controls for playback')
 
     def main(self, *, args):  # noqa: D102
         qos_profile_overrides = {}  # Specify a valid default
@@ -116,6 +122,8 @@ class PlayVerb(VerbExtension):
         play_options.loop = args.loop
         play_options.topic_remapping_options = topic_remapping
         play_options.clock_publish_frequency = args.clock
+        play_options.delay = args.delay
+        play_options.disable_keyboard_controls = args.disable_keyboard_controls
 
         player = Player()
         player.play(storage_options, play_options)
