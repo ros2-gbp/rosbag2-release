a38ec810957e8e8deca35dfde70032152b11e853
Emerson Knapp <emerson.b.knapp@gmail.com>
2022-08-11 14:05:05 -0700

    Rebase from 'upstream'
    Signed-off-by: Emerson Knapp <emerson.b.knapp@gmail.com>

--- rosbag2_cpp/src/rosbag2_cpp/writers/sequential_writer.cpp
+++ rosbag2_cpp/src/rosbag2_cpp/writers/sequential_writer.cpp
@@ -74,6 +74,7 @@ void SequentialWriter::init_metadata()
   file_info.starting_time = std::chrono::time_point<std::chrono::high_resolution_clock>(
     std::chrono::nanoseconds::max());
   file_info.message_count = 0;
+  metadata_.custom_data = storage_options_.custom_data;
   metadata_.files = {file_info};
 }
 
@@ -266,16 +267,23 @@ void SequentialWriter::switch_to_next_storage()
 
 void SequentialWriter::split_bagfile()
 {
+  auto info = std::make_shared<bag_events::BagSplitInfo>();
+  info->closed_file = storage_->get_relative_file_path();
   switch_to_next_storage();
+  info->opened_file = storage_->get_relative_file_path();
 
   metadata_.relative_file_paths.push_back(strip_parent_path(storage_->get_relative_file_path()));
 
   rosbag2_storage::FileInformation file_info{};
+  file_info.starting_time = std::chrono::time_point<std::chrono::high_resolution_clock>(
+    std::chrono::nanoseconds::max());
   file_info.path = strip_parent_path(storage_->get_relative_file_path());
   metadata_.files.push_back(file_info);
+
+  callback_manager_.execute_callbacks(bag_events::BagEvent::WRITE_SPLIT, info);
 }
 
-void SequentialWriter::write(std::shared_ptr<rosbag2_storage::SerializedBagMessage> message)
+void SequentialWriter::write(std::shared_ptr<const rosbag2_storage::SerializedBagMessage> message)
 {
   if (!storage_) {
     throw std::runtime_error("Bag is not open. Call open() before writing.");
@@ -295,10 +303,14 @@ void SequentialWriter::write(std::shared_ptr<rosbag2_storage::SerializedBagMessa
   const auto message_timestamp = std::chrono::time_point<std::chrono::high_resolution_clock>(
     std::chrono::nanoseconds(message->time_stamp));
 
-  if (should_split_bagfile(message_timestamp)) {
-    split_bagfile();
+  if (is_first_message_) {
     // Update bagfile starting time
     metadata_.starting_time = message_timestamp;
+    is_first_message_ = false;
+  }
+
+  if (should_split_bagfile(message_timestamp)) {
+    split_bagfile();
     metadata_.files.back().starting_time = message_timestamp;
   }
 
@@ -306,7 +318,7 @@ void SequentialWriter::write(std::shared_ptr<rosbag2_storage::SerializedBagMessa
 
   metadata_.files.back().starting_time =
     std::min(metadata_.files.back().starting_time, message_timestamp);
-  const auto duration = message_timestamp - metadata_.starting_time;
+  const auto duration = message_timestamp - metadata_.files.back().starting_time;
   metadata_.duration = std::max(metadata_.duration, duration);
 
   const auto file_duration = message_timestamp - metadata_.files.back().starting_time;
@@ -336,9 +348,9 @@ bool SequentialWriter::take_snapshot()
   return true;
 }
 
-std::shared_ptr<rosbag2_storage::SerializedBagMessage>
+std::shared_ptr<const rosbag2_storage::SerializedBagMessage>
 SequentialWriter::get_writeable_message(
-  std::shared_ptr<rosbag2_storage::SerializedBagMessage> message)
+  std::shared_ptr<const rosbag2_storage::SerializedBagMessage> message)
 {
   return converter_ ? converter_->convert(message) : message;
 }
@@ -363,7 +375,7 @@ bool SequentialWriter::should_split_bagfile(
     auto max_duration_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(
       std::chrono::seconds(storage_options_.max_bagfile_duration));
     should_split = should_split ||
-      ((current_time - metadata_.starting_time) > max_duration_ns);
+      ((current_time - metadata_.files.back().starting_time) > max_duration_ns);
   }
 
   return should_split;
@@ -403,6 +415,15 @@ void SequentialWriter::write_messages(
     if (topics_names_to_info_.find(msg->topic_name) != topics_names_to_info_.end()) {
       topics_names_to_info_[msg->topic_name].message_count++;
     }
+  }
+}
+
+void SequentialWriter::add_event_callbacks(const bag_events::WriterEventCallbacks & callbacks)
+{
+  if (callbacks.write_split_callback) {
+    callback_manager_.add_event_callback(
+      callbacks.write_split_callback,
+      bag_events::BagEvent::WRITE_SPLIT);
   }
 }
 
