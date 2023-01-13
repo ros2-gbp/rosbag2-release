d1e799a2386b7bab42c060bf4dae98c002154d00
Audrow Nash <audrow@openrobotics.org>
2023-01-13 14:11:52 -0600

    Rebase from 'upstream'

--- rosbag2_cpp/include/rosbag2_cpp/reader.hpp
+++ rosbag2_cpp/include/rosbag2_cpp/reader.hpp
@@ -67,7 +67,7 @@ public:
    * This must be called before any other function is used.
    *
    * \note This will open URI with the default storage options
-   * * using sqlite3 storage backend
+   * * using default storage backend
    * * using no converter options, storing messages with the incoming serialization format
    * \sa rmw_get_serialization_format.
    * For specifications, please see \sa open, which let's you specify
@@ -104,10 +104,12 @@ public:
    * read head timestamp.
    *
    * \param read_order Sorting criterion and direction to read messages in
+   * \throws runtime_error if the Reader is not open.
+   * \return true if the requested read order has been successfully set.
    * \note Calling set_read_order(order) concurrently with has_next(), seek(t), has_next_file()
-   * or load_next_file() will cause undefined behavior
+   * or load_next_file() will cause undefined behavior.
    */
-  void set_read_order(const rosbag2_storage::ReadOrder & read_order);
+  bool set_read_order(const rosbag2_storage::ReadOrder & read_order);
 
   /**
    * Ask whether the underlying bagfile contains at least one more message.
