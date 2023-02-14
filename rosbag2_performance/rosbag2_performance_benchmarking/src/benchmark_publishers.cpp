101322299d5c3b2616603e86144385fa0674535a
Chris Lalancette <clalancette@gmail.com>
2023-02-14 20:57:05 +0000

    Revert "Trimmed the branch to only the rosbag2_compression_zstd sub directory"
    This reverts commit 24c4088f22d386c16f7e3c657db54fe1f7bc62a1.

---
+++ rosbag2_performance/rosbag2_performance_benchmarking/src/benchmark_publishers.cpp
@@ -0,0 +1,147 @@
+// Copyright 2020-2021, Robotec.ai sp. z o.o.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <exception>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "rosbag2_performance_benchmarking/byte_producer.hpp"
+#include "rosbag2_performance_benchmarking/config_utils.hpp"
+#include "rosbag2_performance_benchmarking/publisher_group_config.hpp"
+#include "msg_utils/message_producer_factory.hpp"
+
+#include "rclcpp/executors/single_threaded_executor.hpp"
+#include "rclcpp/node.hpp"
+#include "rclcpp/qos.hpp"
+#include "rosbag2_performance_benchmarking_msgs/msg/byte_array.hpp"
+
+
+class BenchmarkPublishers : public rclcpp::Node
+{
+public:
+  explicit BenchmarkPublishers(const std::string & name)
+  : rclcpp::Node(name)
+  {
+    create_benchmark_producers();
+    wait_for_subscriptions();
+  }
+
+  void run()
+  {
+    std::vector<std::thread> producer_threads;
+    for (auto & producer : producers_) {
+      producer_threads.emplace_back(
+        [this, producer]() {
+          this->producer_job(producer);
+        });
+    }
+
+    for (auto & thread : producer_threads) {
+      thread.join();
+    }
+  }
+
+private:
+  struct BenchmarkProducer
+  {
+    std::shared_ptr<msg_utils::ProducerBase> msg_producer;
+    std::chrono::milliseconds period;
+    size_t produced_messages = 0;
+    size_t max_messages = 0;
+
+    void produce()
+    {
+      this->msg_producer->produce();
+      ++this->produced_messages;
+    }
+  };
+
+  void wait_for_subscriptions()
+  {
+    if (config_utils::wait_for_subscriptions_from_node_parameters(*this)) {
+      for (auto producer : producers_) {
+        producer->msg_producer->wait_for_matched();
+      }
+    }
+  }
+
+  void create_benchmark_producers()
+  {
+    const auto configurations = config_utils::publisher_groups_from_node_parameters(*this);
+
+    if (configurations.empty()) {
+      RCLCPP_ERROR(get_logger(), "No publishers/producers found in node parameters");
+      return;
+    }
+
+    const std::string node_name(get_fully_qualified_name());
+
+    for (auto & config : configurations) {
+      for (unsigned int i = 0; i < config.count; ++i) {
+        const std::string topic = node_name + "/" + config.topic_root + "_" + std::to_string(i + 1);
+        auto producer = create_benchmark_producer(topic, config);
+        producers_.push_back(producer);
+      }
+    }
+  }
+
+  std::shared_ptr<BenchmarkProducer> create_benchmark_producer(
+    std::string topic,
+    const PublisherGroupConfig & config)
+  {
+    const auto & producer_config = config.producer_config;
+    auto producer = std::make_shared<BenchmarkProducer>();
+
+    producer->msg_producer = msg_utils::create(producer_config.message_type, *this, topic, config);
+    producer->max_messages = producer_config.max_count;
+    producer->period = std::chrono::milliseconds(
+      producer_config.frequency ? 1000 / producer_config.frequency : 1);
+
+    return producer;
+  }
+
+  void producer_job(std::shared_ptr<BenchmarkProducer> producer)
+  {
+    for (auto i = 0u; i < producer->max_messages; ++i) {
+      if (!rclcpp::ok()) {
+        break;
+      }
+      std::this_thread::sleep_for(producer->period);
+      producer->produce();
+    }
+  }
+
+  std::vector<std::shared_ptr<BenchmarkProducer>> producers_;
+};
+
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  auto publishers_node = std::make_shared<BenchmarkPublishers>(
+    "rosbag2_performance_benchmarking_node");
+
+  rclcpp::executors::SingleThreadedExecutor executor;
+  executor.add_node(publishers_node);
+
+  // The benchmark has its own control loop but uses spinning for parameters
+  std::thread spin_thread([&executor]() {executor.spin();});
+  publishers_node->run();
+  RCLCPP_INFO(publishers_node->get_logger(), "Benchmark terminated");
+  rclcpp::shutdown();
+  spin_thread.join();
+
+  return 0;
+}
