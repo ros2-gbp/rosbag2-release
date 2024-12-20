d8973def33db63524e48f57b43d9a4b0675a4450
Marco A. Gutierrez <marcogg@marcogg.com>
2024-12-20 14:35:17 +0000

    Rebase from 'upstream'

--- ros2bag/ros2bag/verb/info.py
+++ ros2bag/ros2bag/verb/info.py
@@ -30,34 +30,21 @@ class InfoVerb(VerbExtension):
             '-v', '--verbose', action='store_true',
             help='Display request/response information for services'
         )
-
-    def _is_service_event_topic(self, topic_name, topic_type) -> bool:
-
-        service_event_type_middle = '/srv/'
-        service_event_type_postfix = '_Event'
-
-        if (service_event_type_middle not in topic_type
-                or not topic_type.endswith(service_event_type_postfix)):
-            return False
-
-        service_event_topic_postfix = '/_service_event'
-        if not topic_name.endswith(service_event_topic_postfix):
-            return False
-
-        return True
+        available_sorting_methods = Info().get_sorting_methods()
+        parser.add_argument(
+            '--sort', default='name', choices=available_sorting_methods,
+            help='Configuration for sorting of output. '
+                 'By default sorts by topic name - use this argument to override.')
 
     def main(self, *, args):  # noqa: D102
         if args.topic_name and args.verbose:
             print("Warning! You have set both the '-t' and '-v' parameters. The '-t' parameter "
                   'will be ignored.')
+        m = Info().read_metadata(args.bag_path, args.storage)
         if args.verbose:
-            Info().read_metadata_and_output_service_verbose(args.bag_path, args.storage)
+            Info().print_output_verbose(args.bag_path, m, args.sort)
         else:
-            m = Info().read_metadata(args.bag_path, args.storage)
             if args.topic_name:
-                for topic_info in m.topics_with_message_count:
-                    if not self._is_service_event_topic(topic_info.topic_metadata.name,
-                                                        topic_info.topic_metadata.type):
-                        print(topic_info.topic_metadata.name)
+                Info().print_output_topic_name_only(m, args.sort)
             else:
-                print(m)
+                Info().print_output(m, args.sort)
